08ecf1fc51d2baadb106e81653314d95b8316258
Eli Wood <mreliwood1@gmail.com>
2023-01-12 14:59:57 -0800

    undo sequelize testing

--- config/connection.js
+++ config/connection.js
@@ -5,15 +5,15 @@ require('dotenv').config();
 let sequelize;
 
 // database selection
-// if (process.env.CYCLIC_DB) {
-// 	sequelize = new Sequelize(process.env.CYCLIC_DB);
-// } else {
-// 	sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-// 		host: 'localhost',
-// 		dialect: 'mysql',
-// 		port: 3306
-// 	});
-// }
+if (process.env.CYCLIC_DB) {
+	sequelize = new Sequelize(process.env.CYCLIC_DB);
+} else {
+	sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+		host: 'localhost',
+		dialect: 'mysql',
+		port: 3306
+	});
+}
 
 // export
 module.exports = sequelize;
