cec3141181b7f6330f91a535847a2bc17e842568
Eli Wood <mreliwood1@gmail.com>
2023-01-12 14:30:12 -0800

    adding cyclic db

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 let sequelize;
 
 // database selection
-if (process.env.JAWSDB_URL) {
-	sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.CYCLIC_DB) {
+	sequelize = new Sequelize(process.env.CYCLIC_DB);
 } else {
 	sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
 		host: 'localhost',
